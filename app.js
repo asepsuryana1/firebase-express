82833b9221f8b6a9a3af9f1feb2b7aa32a3ca1a4
asepsuryana1 <asepsuryana1@gmail.com>
2020-11-26 08:57:32 +0700

    setting config

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var logger = require('morgan');
 var firebase = require('firebase');
 
 var config = {
-    apiKey: '<your-api-key>',
-    authDomain: '<your-auth-domain>',
-    databaseURL: '<your-database-url>',
-    projectId: 'firstproject-f2f04',
-    storageBucket: '<your-storage-bucket>',
-    messagingSenderId: '<your-sender-id>'
+    apiKey: 'AIzaSyDXcG2PN2nO1xcLcxW8gtHbomzNP95E7Nc',
+    authDomain: 'nuuby-co.firebaseio.com',
+    databaseURL: 'https://nuuby-co.firebaseio.com',
+    projectId: 'nuuby-co',
+    storageBucket: 'nuuby-co.app.spot.com',
+    messagingSenderId: '850383414810'
   };
 
   firebase.initializeApp(config)
