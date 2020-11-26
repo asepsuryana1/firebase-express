44764f8df4ba0d456841996614a26308f02dbc76
asepsuryana1 <asepsuryana1@gmail.com>
2020-11-26 10:19:22 +0700

    testing get succes 'postman' testing post success

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,36 @@ var router = express.Router();
 var firebase = require('firebase');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  //Fetch instances
+router.get('/', function (req, res) {
+  const userReference = firebase.database().ref("/Users/");
+  //Attach an asynchronous callback to read the data
+  userReference.on("value", function(snapshot) {
+    console.log(snapshot.val());
+    res.json(snapshot.val());
+    userReference.off("value");
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+    res.send("The read failed: " + errorObject.code);
+  });
 });
+
+router.post('/', function (req, res) {
+  const userName = req.body.username;
+  const name = req.body.name;
+  const age = req.body.age;
+
+  const referencePath = '/Users/'+userName+'/';
+  const userReference = firebase.database().ref(referencePath);
+  userReference.set({Name: name, Age: age}, function(error) {
+    if (error) {
+      res.send("Data could not be saved." + error);
+    } else {
+      res.send("Data saved successfully.");
+    }
+  });
+});
+
 
 module.exports = router;
